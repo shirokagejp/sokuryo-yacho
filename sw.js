b74197b7235d9ca04777d0eaa8c147add0b8916c
shirokagejp <k-ootori@mocha.ocn.ne.jp>
2026-04-16 14:33:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,16 @@
-const CACHE = 'sokuryo-v2';
-const FILES = [
-  './index.html',
-  './manifest.json'
-];
-
+// 古いキャッシュを全部消してSWを無効化する
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
-  );
   self.skipWaiting();
 });
-
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() {
+      return self.clients.claim();
     })
   );
-  self.clients.claim();
 });
-
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request).then(function(res) {
-        return caches.open(CACHE).then(function(c) {
-          c.put(e.request, res.clone());
-          return res;
-        });
-      }).catch(function() { return caches.match('./index.html'); });
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
